9eaa01cb43d570adee93e711b3955cebdfaecfb2
FrancisFYK <2114009146@qq.com>
2017-03-31 13:46:00 +0800

    增加CustomMessageHandler和CustomMessageContext

---
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/MessageHandlers/CustomMessageHandler/CustomMessageHandler.cs
@@ -0,0 +1,147 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+
+    文件名：CustomMessageHandler.cs
+    文件功能描述：微信公众号自定义MessageHandler
+
+
+    创建标识：Senparc - 20170331
+----------------------------------------------------------------*/
+
+using System.Collections.Generic;
+using System.IO;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MessageHandlers;
+
+namespace Senparc.Weixin.MP.Sample.CommonService.CustomMessageHandler
+{
+    /// <summary>
+    /// 自定义MessageHandler
+    /// 把MessageHandler作为基类，重写对应请求的处理方法
+    /// </summary>
+    public partial class CustomMessageHandler : MessageHandler<CustomMessageContext>
+    {
+
+
+        private string appId = ""; /*WebConfigurationManager.AppSettings["WeixinAppId"];*/
+        private string appSecret = "";/* WebConfigurationManager.AppSettings["WeixinAppSecret"];*/
+
+        /// <summary>
+        /// 模板消息集合（Key：checkCode，Value：OpenId）
+        /// </summary>
+        public static Dictionary<string, string> TemplateMessageCollection = new Dictionary<string, string>();
+
+        public CustomMessageHandler(Stream inputStream, PostModel postModel, int maxRecordCount = 0)
+            : base(inputStream, postModel, maxRecordCount)
+        {
+            //这里设置仅用于测试，实际开发可以在外部更全局的地方设置，
+            //比如MessageHandler<MessageContext>.GlobalWeixinContext.ExpireMinutes = 3。
+            WeixinContext.ExpireMinutes = 3;
+
+            if (!string.IsNullOrEmpty(postModel.AppId))
+            {
+                appId = postModel.AppId;//通过第三方开放平台发送过来的请求
+            }
+
+            //在指定条件下，不使用消息去重
+            base.OmitRepeatedMessageFunc = requestMessage =>
+            {
+                var textRequestMessage = requestMessage as RequestMessageText;
+                if (textRequestMessage != null && textRequestMessage.Content == "容错")
+                {
+                    return false;
+                }
+                return true;
+            };
+        }
+
+        public CustomMessageHandler(RequestMessageBase requestMessage)
+            : base(requestMessage)
+        {
+        }
+
+        public override void OnExecuting()
+        {
+            //测试MessageContext.StorageData
+            if (CurrentMessageContext.StorageData == null)
+            {
+                CurrentMessageContext.StorageData = 0;
+            }
+            base.OnExecuting();
+        }
+
+        public override void OnExecuted()
+        {
+            base.OnExecuted();
+            CurrentMessageContext.StorageData = ((int)CurrentMessageContext.StorageData) + 1;
+        }
+
+        /// <summary>
+        /// 处理图片请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public override IResponseMessageBase OnImageRequest(RequestMessageImage requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageNews>();
+            responseMessage.Articles.Add(new Article()
+            {
+                Title = "您刚才发送了图片信息",
+                Description = "您发送的图片将会显示在边上",
+                PicUrl = requestMessage.PicUrl,
+                Url = "http://sdk.weixin.senparc.com"
+            });
+            responseMessage.Articles.Add(new Article()
+            {
+                Title = "第二条",
+                Description = "第二条带连接的内容",
+                PicUrl = requestMessage.PicUrl,
+                Url = "http://sdk.weixin.senparc.com"
+            });
+
+            return responseMessage;
+        }
+
+        /// <summary>
+        /// 处理链接消息请求
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public override IResponseMessageBase OnLinkRequest(RequestMessageLink requestMessage)
+        {
+            var responseMessage = ResponseMessageBase.CreateFromRequestMessage<ResponseMessageText>(requestMessage);
+            responseMessage.Content = string.Format(@"您发送了一条连接信息：
+Title：{0}
+Description:{1}
+Url:{2}", requestMessage.Title, requestMessage.Description, requestMessage.Url);
+            return responseMessage;
+        }
+
+        /// <summary>
+        /// 处理事件请求（这个方法一般不用重写，这里仅作为示例出现。除非需要在判断具体Event类型以外对Event信息进行统一操作
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public override IResponseMessageBase OnEventRequest(IRequestMessageEventBase requestMessage)
+        {
+            var eventResponseMessage = base.OnEventRequest(requestMessage);//对于Event下属分类的重写方法，见：CustomerMessageHandler_Events.cs
+            //TODO: 对Event信息进行统一操作
+            return eventResponseMessage;
+        }
+
+        public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        {
+            /* 所有没有被处理的消息会默认返回这里的结果，
+            * 因此，如果想把整个微信请求委托出去（例如需要使用分布式或从其他服务器获取请求），
+            * 只需要在这里统一发出委托请求，如：
+            * var responseMessage = MessageAgent.RequestResponseMessage(agentUrl, agentToken, RequestDocument.ToString());
+            * return responseMessage;
+            */
+
+            var responseMessage = this.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "这条消息来自DefaultResponseMessage。";
+            return responseMessage;
+        }
+    }
+}
